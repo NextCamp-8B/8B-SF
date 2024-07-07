76886201e56a22aa17dadda5e38dca5ce8ddc201
unknown <gjstkd123@naver.com>
2024-07-07 17:20:27 +0900

    feat #152 : header 1200px 고정
    - header 1200px 고정

--- src/app/(before)/layout.tsx
+++ src/app/(before)/layout.tsx
@@ -12,9 +12,11 @@ Readonly<{
       className={`w-full min-h-dvh bg-background-100 pt-32 pb-20 px-14 text-grayscale-900 flex justify-center`}
     >
       <div className="w-full min-w-[1200px] h-[80px] flex items-center px-28 bg-[#FFFFFF] fixed top-0 z-50">
-        <Link href="/login">
-          <Logo />
-        </Link>
+        <div className="w-[1200px] flex items-center mx-auto">
+          <Link href="/login">
+            <Logo />
+          </Link>
+        </div>
       </div>
       {children}
       {/* 병렬,인터셉트 라우트 활용해서 임시발급 비밀번호 모달창 발생*/}
